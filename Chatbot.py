ea049cc7d0883d136b70336aff6482fa347215f7
Akhilesh Tiwari <akhileshtiwari24@gmail.com>
2024-08-26 23:07:01 -0400

    Update Chatbot.py
    add sum function for testing

--- Chatbot.py
+++ Chatbot.py
@@ -27,3 +27,6 @@ if prompt := st.chat_input():
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
+
+def sum(n1, n2):
+    return n1
